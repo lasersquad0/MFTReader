0c9c653baa971eab2e540b5b74a44a4351fffe6e
lasersquad0 <lasersquad@gmail.com>
2025-07-09 22:04:35 +0300

    .

--- include/Functions.h
+++ include/Functions.h
@@ -23,23 +23,21 @@
 
 #define GetAttrName(pRec, field)  ( (wchar_t*)((uint8_t*)(pRec) + (pRec->field)) )
 #define GetFName(pRec, offset) ( (wchar_t*)((uint8_t*)(pRec) + (offset)) )
-#define IsDir(_) (((_).dup.FileAttrib & (uint32_t)FILE_ATTR_FLAGS::DIRECTORY) > 0)
-#define IsItemDir(_) (((_).FileAttr & (uint32_t)FILE_ATTR_FLAGS::DIRECTORY) > 0)
-#define IsReparse(_) (((_).dup.FileAttrib & (uint32_t)FILE_ATTR_FLAGS::REPARSE_POINT) > 0)
-#define IsMetaFile(_) (((_).Attr.ParentDir.sId.low == 5) && ((_).ciName[0] == L'$'))
-#define IsDotDir(_) ( ((_)[0] == L'.') && ((_).size() == 1) )
+#define AttrMetaFile(_) ( ((_)->ParentDir.sId.low == MFT_ROOT_REC_ID) && (GetFName(_, sizeof(*_))[0] == L'$') )
+#define AttrDotDir(_) ( ((_)->FileNameLen == 1) && (GetFName(_, sizeof(*_))[0] == L'.')  )
+#define AttrNTfsInternal(_) (AttrMetaFile(_) || AttrDotDir(_))
 
 // ANSI chaset for logging purposes only
 #define ATTR_TYPE_NAMES { "ZERO", "STANDARD INFO", "ATTR LIST", "FILENAME", "OBJECT ID", "secure_info", "LABEL", \
                           "VOLUME INFO", "DATA", "INDEX ROOT", "ALLOCATION", "BITMAP", "REPARSE", "EA INFORMATION", \
-                          "EA", "PROPERTYSHEET", "LOGGED UTILITY STREAM" }
+                          "EA", "PROPERTYSHEET", "LOGGED UTILITY STREAM", "USER ATTRIBUTE" }
 
-// Attr types have numbers 0x10 0x20 0x100, etc. - convert them into consecutive indexes in the array
+// Attr types have numbers 0x10, 0x20, 0x30, etc. - convert them into consecutive indexes in the array
 // used for indexing ATTR_TYPE_NAMES array and in some other places
 #define MakeAttrTypeIndex(_) ((_)>>4) 
 #define AttrName(_) (AttrTypeNames[MakeAttrTypeIndex(_)])
 
-static const char* AttrTypeNames[] ATTR_TYPE_NAMES;
+static const char* AttrTypeNames[ATTR_TYPE_CNT] ATTR_TYPE_NAMES;
 static const char* FileNameTypes[]{ "POSIX", "UNICODE", "DOS", "UNICODE_AND_DOS" };
 
 struct VOLUME_DATA
@@ -62,6 +60,12 @@ struct FILE_NAME
     //bool operator>(const FILE_NAME& other) const { return ciName > other.ciName; }
     bool operator<(const FILE_NAME& other) const { return ciName < other.ciName; }
     bool operator==(const FILE_NAME& other) const { return ciName == other.ciName; }
+
+    bool IsDir() const { return (Attr.dup.FileAttrib & (uint32_t)FILE_ATTR_FLAGS::DIRECTORY) > 0; }
+    bool IsMetaFile() const { return (Attr.ParentDir.sId.low == MFT_ROOT_REC_ID) && (ciName[0] == L'$'); }
+    bool IsDotDir() const { return (ciName.size() == 1) && (ciName[0] == L'.'); }
+    bool IsReparse() const { return (Attr.dup.FileAttrib & (uint32_t)FILE_ATTR_FLAGS::REPARSE_POINT) > 0; };
+    bool NtfsInternal() const { return IsMetaFile() || IsDotDir(); }
 };
 
 typedef THArray<FILE_NAME> TFileList;
@@ -87,29 +91,35 @@ struct ITEM_INFO
 {
     MFT_REF RecID;
     uint16_t AttrCounters[ATTR_TYPE_CNT];
-    bool NonResidentAttrList;
-    bool NonResidentBitmap;
-    bool NonResidentAlloc;
-    bool ResidentData;
-    uint32_t FileAttr;
+    bool NonResidentAttrList; // rare case. has an ATTR_LIST attribute that is non-resident
+    bool NonResidentBitmap;   // rare case. has an BITMAP attribute that is non-resident
+    //bool NonResidentAlloc;    
+    bool ResidentData;   // Has an DATA attribute that is resident. Not so rare case.
+    uint32_t FileAttrib;
 
-    uint HardLinksCount;
-    uint AttrsCount;
-    uint FileNamesCount; // all types - DOS, WIN, POSIX
-    uint DataStreamsCount;
-    std::wstring MainName;
-    THArray<std::wstring> FileNames;
-    std::wstring DataStreamNames[20]; // unlikely that file will have more than 5 file streams
+    uint16_t HardLinksCount;
+    uint16_t AttrsCount;
+    ci_string MainName; // here is ci_string for quicker sorting array of ITEM_INFO
+    THArray<std::wstring> FileNames; // contains filenames of all types - DOS, WIN and POSIX
+    THash<std::wstring, uint16_t> DataStreamNames; // data stream name counts groupped by stream name
     
     DIR_NODE Node;
 
     bool operator<(const ITEM_INFO& other) const 
     { 
-        ci_string n1 = MainName.c_str();
+        /*ci_string n1 = MainName.c_str();
         ci_string n2 = other.MainName.c_str();
-        return n1 < n2; 
+        return n1 < n2;*/
+        return MainName < other.MainName;
     }
+
     bool operator==(const ITEM_INFO& other) const { return RecID.Id == other.RecID.Id; }
+
+    bool IsDir() const { return (FileAttrib & (uint32_t)FILE_ATTR_FLAGS::DIRECTORY) > 0; }
+//    bool IsMetaFile() const { return (Attr.ParentDir.sId.low == MFT_ROOT_REC_ID) && (ciName[0] == L'$'); }
+//    bool IsDotDir() const { return (ciName.size() == 1) && (ciName[0] == L'.'); }
+//    bool NtfsInternal() const { return IsMetaFile() || IsDotDir(); }
+
 };
 
 typedef THArray<ITEM_INFO> TItemInfoList;
@@ -122,19 +132,21 @@ bool ParseNonresBitmap(const VOLUME_DATA& volData, MFT_ATTR_HEADER* attr, TBitFi
 bool ParseNonresAttrList(const VOLUME_DATA& volData, MFT_ATTR_HEADER* attrAttrList, ATTR_TYPE attrType, PMFT_ATTR_HEADER* result);
 bool ReadDirectoryV1(VOLUME_DATA& volData, /*MFT_REF mftRecID,*/ uint32_t parentIdx, CACHE_ITEM* parentItem /*uint32_t levelIdx*/, uint64_t& dirSize, TFileCache& gFileList);
 bool ReadDirectoryV2(VOLUME_DATA& volData, MFT_REF parentMftRecID, uint32_t dirLevel, TFileList& gDirList);
-bool DataRunsDecode(MFT_ATTR_HEADER* attr, THArray<DATA_RUN_ITEM>& runs);
-bool ReadClusters(const VOLUME_DATA& volData, uint64_t lcnStart, uint32_t lcnCnt, PBYTE dataBuf);
-bool FixupUSA(const VOLUME_DATA& volData, PBYTE dataBuf, DATA_RUN_ITEM& rli, uint32_t rlilen);
+bool DataRunsDecode(MFT_ATTR_HEADER* attr, TDataRuns& runs);
+bool ReadClusters(const VOLUME_DATA& volData, CLST lcnStart, CLST lcnCnt, PBYTE dataBuf);
+bool FixupUSA(const VOLUME_DATA& volData, PBYTE dataBuf, DATA_RUN_ITEM& rli, uint64_t rlilen);
 bool LoadMFTRecord(const VOLUME_DATA& volData, MFT_REF recID, uint8_t* mftRec);
 uint8_t* LoadMFTRecordCache(const VOLUME_DATA& volData, MFT_REF recID);
 void GetAttr(const VOLUME_DATA& volData, ATTR_TYPE attrType, const PMFT_ATTR_HEADER* const attrValues, PMFT_ATTR_HEADER* result);
 void FillAttrValues(MFT_FILE_RECORD* mftRec, PMFT_ATTR_HEADER* attrValues);
 //int32_t GetFileListFromMFTRec(const VOLUME_DATA& volData, MFT_FILE_RECORD* mftRec, DIR_NODE& node);
-bool ReadMftItemInfo(VOLUME_DATA& volData, MFT_REF parentDirRecID, uint32_t dirLevel, ITEM_INFO& itemInfo);
 void GetFileList(INDEX_HDR* ihdr, FileListPred pred);
 bool ProcessAllocDataRuns(VOLUME_DATA& volData, DIR_NODE& node, FileListPred pred);
 void ReadDirsV2(VOLUME_DATA& volData);
 void ReadDirsV1(VOLUME_DATA& volData);
 void ReadItems(VOLUME_DATA& volData);
 uint32_t MFTRecIdByPath(VOLUME_DATA& volData, const ci_string& path);
+bool ReadMftItemInfo(VOLUME_DATA& volData, MFT_REF mftRecRef, ITEM_INFO& itemInfo);
+
+
 //void GetFileListFromNode(INDEX_HDR* ihdr, TLCNRecs& lcns, TFileList& fnames);
