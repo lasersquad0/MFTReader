57116dedac3c4416f8db77dbcc8586844cbfe97e
lasersquad0 <lasersquad@gmail.com>
2026-06-11 12:36:20 +0300

    added file Ticks.h

---
+++ include/external/strutils/include/Ticks.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <chrono>
+#include <iostream>
+#include "string_utils.h"
+
+//std::string millisecToStr(long long ms);
+
+class Ticks
+{
+public:
+	typedef std::chrono::steady_clock::time_point timepoint;
+
+private:
+	static inline std::map<string_t, timepoint> s;
+	static inline std::map<string_t, timepoint> f;
+
+public:
+
+	static void Start(const string_t& tickName)
+	{
+		s[tickName] = std::chrono::steady_clock::now();
+	}
+	static long long Finish(const string_t& tickName)
+	{
+		f[tickName] = std::chrono::steady_clock::now();
+		return GetTick(tickName);
+	}
+	static long long GetTick(const string_t& tickName)
+	{
+		timepoint& fin = f.at(tickName);
+		return std::chrono::duration_cast<std::chrono::milliseconds>(fin - s.at(tickName)).count();
+	}
+	static void PrintCon(long factor)
+	{
+		for (auto& item : f)
+		{
+			std::wcout << item.first << U("=") << std::chrono::duration_cast<std::chrono::milliseconds>(item.second - s.at(item.first)).count() / factor << std::endl;
+		}
+	}
+	static void Print(std::basic_iostream<char_t>& stream, long factor)
+	{
+		for (auto& item : f)
+		{
+			stream << item.first << U("=") << std::chrono::duration_cast<std::chrono::milliseconds>(item.second - s.at(item.first)).count() / factor << std::endl;
+		}
+	}
+	
+};
+
