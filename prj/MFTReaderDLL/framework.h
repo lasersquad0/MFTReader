343efd3ca1b4f62ef73a5a3c7ea51fc34dcef871
lasersquad0 <lasersquad@gmail.com>
2026-06-23 00:07:36 +0300

    added include "Debug.h" on top of file

--- prj/MFTReaderDLL/framework.h
+++ prj/MFTReaderDLL/framework.h
@@ -2,6 +2,7 @@
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 
+#include "Debug.h"
 #include <windows.h>
 #include <shlwapi.h>
 #include <strutils/include/string_utils.h>
