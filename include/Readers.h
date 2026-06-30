713f16587e68b19a99bfc65f60d9f4a47e61c7d3
lasersquad0 <lasersquad@gmail.com>
2026-06-30 16:06:06 +0300

    new files

---
+++ include/Readers.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "Functions.h"
+
+class TMFTReaderBase
+{
+protected:
+	VOLUME_DATA FVolumeData;
+public:
+	string_t ParseVolume(const string_t& vol);
+	void ReadVolumeData(const string_t& vol);  // volume should be in format \\.\c:
+
+};
+
+class TMFTStatReader : public TMFTReaderBase
+{
+private:
+	TItemInfoList FItemsList;
+public:
+	TMFTStatReader();
+	TMFTStatReader(string_t& volume); // throws an exception in case of cannot open volume
+
+	//functions for reading ALL MFT records info and build statistics
+	bool ReadMftItems(MFT_REF startMmftRec, uint32_t dirLevel);
+	bool ReadMftItemInfo(MFT_REF mftRecRef, ITEM_INFO& itemInfo);
+	void ShowVolumeStat();
+
+};
+
+class TMFTSearchReader: public TMFTReaderBase
+{
+public:
+	TMFTSearchReader();
+	TMFTSearchReader(string_t& volume); // throws an exception in case of cannot open volume
+
+	bool ReadDirectoryV1(uint32_t parentIdx, CACHE_ITEM* parentItem, uint64_t& dirSize, TFileCache& gFileList, ProgressCallbackPtr callback);
+	void ReadDirsV1();
+};
+
