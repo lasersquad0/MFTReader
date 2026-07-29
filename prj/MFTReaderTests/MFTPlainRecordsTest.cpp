19d2f82afd1148df899e6c1fac00dddf30986f8b
lasersquad0 <lasersquad@gmail.com>
2026-07-29 23:33:14 +0300

    new error handling system implemented (TErrorCode instead of bool)

--- prj/MFTReaderTests/MFTPlainRecordsTest.cpp
+++ prj/MFTReaderTests/MFTPlainRecordsTest.cpp
@@ -4,15 +4,15 @@
 #include "TestUtils.h"
 #include "MFTBaseParamTest.h"
 
-// reads file with MFT records
-// file has no internal structure, just MFT records one-by-one
+// reads files containing just MFT records (.mft)
+// these are files where first bytes in a file are first bytes of first MFT record
+// such files do not have internal structure, just MFT records one-by-one
 class TMFTPlainRecordsLoader : public IRecordLoader
 {
 private:
     std::ifstream FFile;
-    uint64_t FMFTRecordsCount;
+    uint64_t FMFTRecordsCount = 0;
 public:
-    TMFTPlainRecordsLoader() {}
     TMFTPlainRecordsLoader(const string_t& fileName) { OpenVolume(fileName); }
     ~TMFTPlainRecordsLoader() { CloseVolume(); }
 
@@ -20,6 +20,19 @@ public:
 
     void OpenVolume(const string_t& fileName) override
     {
+        string_t volName;
+        volName.resize(MAX_PATH);
+        
+        BOOL r = GetVolumePathName(fileName.c_str(), volName.data(), MAX_PATH);
+        ASSERT_NE(0, r);
+
+        volName.resize(volName.find(_T('\0')));
+        if (volName[volName.size() - 1] == '\\')
+        {
+            volName[volName.size() - 1] = _T('\0');
+            volName.resize(volName.size() - 1);
+        }
+
         FFile.open(fileName, std::ios::binary);
         if (!FFile) FAIL() << "Error opening file '" << fileName << "'";
         
@@ -33,15 +46,16 @@ public:
         FVolumeData.hVolume = INVALID_HANDLE_VALUE;
         FVolumeData.MftZoneStart.QuadPart = 0;
         FVolumeData.MftZoneEnd.QuadPart = 1000;
-        FVolumeData.Name = std::filesystem::absolute(fileName); // make path absolute (fully qualified and without . and .. )
+        FVolumeData.Name = volName; //std::filesystem::absolute(fileName); // make path absolute (fully qualified and without . and .. )
 
         // reading MFT record #0, getting $MFT LCNs
         uint8_t* mftRecBuf = (uint8_t*)alloca(FVolumeData.BytesPerMFTRec);
         MFT_FILE_RECORD* mftRec = (MFT_FILE_RECORD*)mftRecBuf;
         MFT_REF mftRef{ 0 };
 
-        bool res = LoadMFTRecord(mftRef, mftRecBuf); // loading MFT record #0 which is $MFT file
-        ASSERT_TRUE(res) << "Error loading MFT record " << mftRef.sId.low;
+        FMFTRecordsCount = 1; //temporary
+        TErrorCode res = LoadMFTRecord(mftRef, mftRecBuf); // loading MFT record #0 which is $MFT file
+        ASSERT_EQ(TErrorCode::Success, res) << "Error loading MFT record " << mftRef.sId.low;
 
         if (!ntfs_is_file_recp(mftRec->RecHeader.Signature))
             FAIL() << "MFT record with incorrect signature found " << mftRec->RecHeader.Signature << " (expected 'FILE')";
@@ -52,13 +66,13 @@ public:
         TMFTParserBase prsr(*this);
         TAttrCollection collection;
         res = prsr.FillAttrCollection(mftRec, collection);
-        ASSERT_TRUE(res) << "Error parsing attributes in MFT record buffer " << mftRef.sId.low;
+        ASSERT_EQ(TErrorCode::Success, res) << "Error parsing attributes in MFT record buffer " << mftRef.sId.low;
         auto attr = collection.Get(ATTR_DATA);
         ASSERT_NE(nullptr, attr);
 
         TDataRuns dataRuns;
         res = prsr.DecodeDataRuns(attr, dataRuns);
-        ASSERT_TRUE(res);
+        ASSERT_EQ(TErrorCode::Success, res);
         ASSERT_EQ(1ul, dataRuns.Count());
 
         FMFTRecordsCount = dataRuns[0].len * FVolumeData.BytesPerCluster / FVolumeData.BytesPerMFTRec;
@@ -70,10 +84,17 @@ public:
         IRecordLoader::CloseVolume();
     }
 
-    //returns false when all records are loaded from a file
-    bool LoadMFTRecord(MFT_REF mftRecRef, uint8_t* mftRecData) override
+    // returns error NotFound error when all records are loaded from a file
+    // also it sets eofbit bit in the FFile
+    TErrorCode LoadMFTRecord(MFT_REF mftRecRef, uint8_t* mftRecData) override
     {
         assert(!FFile.fail());
+
+        if (mftRecRef.sId.low >= FMFTRecordsCount)
+        {
+            FFile.setstate(std::ios_base::eofbit);
+            return TErrorCode::NotFound;
+        }
 
         try
         {
@@ -86,10 +107,10 @@ public:
             GET_LOGGER;
             logger.ErrorFmt("[LoadMFTRecord] FFile.seekg() has failed with error: {}", ec.value());
 
-            return false;
+            return TErrorCode::IOError;
         }
 
-        return true;
+        return TErrorCode::Success;
 
         /*
         FFile.seekg(FVolumeData.BytesPerMFTRec * (uint64_t)mftRecRef.sId.low, std::ios::beg);
@@ -105,7 +126,7 @@ public:
         //TODO shall we add FixupUSA1 call here?
     }
 
-    bool ReadClusters(CLST lcnStart, CLST lcnCnt, uint8_t* dataBuf) override
+    TErrorCode ReadClusters(CLST lcnStart, CLST lcnCnt, uint8_t* dataBuf) override
     {
         try
         {
@@ -118,7 +139,7 @@ public:
             GET_LOGGER;
             logger.ErrorFmt("[ReadClusters] FFile.seekg() has failed with error: {}", ec.value());
 
-            return false;
+            return TErrorCode::IOError;
         }
 
 
@@ -137,11 +158,15 @@ public:
             return false;
         }
         */
-        return true;
+        return TErrorCode::Success;
     }
 
 }; //TMFTPlainRecordsLoader
 
+
+#define COMPRESSED_SPARSE_FILE _T(TEST_DATA_DIR "compressed_sparse.mft")
+#define DELETED_FILE           _T(TEST_DATA_DIR "deleted.mft")
+#define DIFFERENT_LA_FILE      _T(TEST_DATA_DIR "different_la.mft")
 
 class MFTPlainRecordsTest : public MFTStringParamTest
 {
@@ -165,6 +190,12 @@ public:
     // To access the test parameter, call GetParam() from class TestWithParam<T>.
 };
 
+static THash<string_t, uint32_t> NotInUseCounts{
+    {COMPRESSED_SPARSE_FILE, 220},
+    {DELETED_FILE, 221 },
+    {DIFFERENT_LA_FILE, 217}
+};
+
 TEST_P(MFTPlainRecordsTest, ReadMftItemInfoBuf_1)
 {
     auto& fileName = GetParam();
@@ -174,29 +205,33 @@ TEST_P(MFTPlainRecordsTest, ReadMftItemInfoBuf_1)
     uint8_t buf[DEFAULT_BYTES_PER_MFT_REC];
     MFT_FILE_RECORD* mftRec = (MFT_FILE_RECORD*)buf;
     MFT_REF mftRef{ 0 };
+    uint32_t notInUseCount = 0;
 
     while (!ldr.Eof())
     {
         ITEM_INFO item; // item declaration should be here in the loop   
 
-        if (/*(mftRef.sId.low != 0) && */(mftRef.sId.low != 9) && (mftRef.sId.low != 24) &&
-            (mftRef.sId.low != 25) /*&& (mftRef.sId.low != 26) */ )
+        if ((mftRef.sId.low != 9) && (mftRef.sId.low != 24) && (mftRef.sId.low != 25) )
         {
-            bool res;
-            res = ldr.LoadMFTRecord(mftRef, buf);
+            TErrorCode res = ldr.LoadMFTRecord(mftRef, buf);
             if (ldr.Eof()) break;
-            EXPECT_TRUE(res);
+            
+            EXPECT_EQ(TErrorCode::Success, res);
 
             if (!ntfs_is_file_recp(mftRec->RecHeader.Signature) && !ntfs_is_magicp(mftRec->RecHeader.Signature, zero))
                 FAIL() << "MFT record with incorrect signature found " << mftRec->RecHeader.Signature << " (neither 'FILE' nor '0000')";
 
             // parse only 'IN USE' records, bypass free ones
-            if (mftRec->Flags & MFT_FLAG_IN_USE)
+            if ((mftRec->Flags & MFT_FLAG_IN_USE) == MFT_FLAG_IN_USE)
             {
                 // parse only 'FILE' records
                 ASSERT_TRUE(ntfs_is_file_recp(mftRec->RecHeader.Signature));
                 res = stat.ReadMftItemInfoBuf(mftRec, item);
-                EXPECT_TRUE(res);
+                EXPECT_EQ(TErrorCode::Success, res);
+            }
+            else
+            {
+                notInUseCount++;
             }
 
             // when assert() fails inside calling function then Google Test aborts immediately, and do not execute remaining tests
@@ -206,6 +241,10 @@ TEST_P(MFTPlainRecordsTest, ReadMftItemInfoBuf_1)
 
         mftRef.sId.low++;
     }
+
+    // check that we processed all available in .mft files records.
+    ASSERT_EQ(256ul, mftRef.sId.low);
+    EXPECT_EQ(NotInUseCounts[fileName], notInUseCount);
 }
 
 TEST_P(MFTPlainRecordsTest, ReadMftItemInfo_1)
@@ -215,37 +254,49 @@ TEST_P(MFTPlainRecordsTest, ReadMftItemInfo_1)
     TMFTStatCollector stat(ldr, false); // DO NOT process non-resident attributes because .mft files do not contain this info
 
     MFT_REF mftRef{ 0 };
-    bool res;
+    TErrorCode res;
+    uint32_t notInUseCount = 0;
 
-    while (true)
+    while (!ldr.Eof())
     {
         ITEM_INFO item; // item declaration should be here in the loop   
 
-        if (/*(mftRef.sId.low != 0) && */(mftRef.sId.low != 9) && (mftRef.sId.low != 24) &&
-            (mftRef.sId.low != 25) /*&& (mftRef.sId.low != 26) */)
+        if ((mftRef.sId.low != 9) && (mftRef.sId.low != 24) && (mftRef.sId.low != 25) )
         {
             res = stat.ReadMftItemInfo(mftRef, item);
             if (ldr.Eof()) break;
-            EXPECT_TRUE(res);
+
+            if (res == TErrorCode::MFTRecordNotInUse) // bypass not in use records
+            {
+                notInUseCount++;
+            }
+            else
+            {
+                EXPECT_EQ(TErrorCode::Success, res);
+            }
         }
 
         mftRef.sId.low++;
     }
+
+    // check that we processed all available in .mft files records.
+    ASSERT_EQ(256ul, mftRef.sId.low);
+    EXPECT_EQ(NotInUseCounts[fileName], notInUseCount);
 }
 
-TEST_P(MFTPlainRecordsTest, DISABLED_GetMFTRecIdByPath_1)
+TEST_P(MFTPlainRecordsTest, GetMFTRecIdByPath_1)
 {
     auto& fileName = GetParam();
     TMFTPlainRecordsLoader ldr(fileName);
     TMFTParserBase ps(ldr);
-
+   
     THArray<std::pair<uint32_t, ci_string>> testData{
         {0, _T("c:\\$MFT")},
         {MFT_ROOT_REC_ID, _T("C")}, {MFT_ROOT_REC_ID, _T("C:")}, { MFT_ROOT_REC_ID, _T("C:\\")},
         {0, _T("")}, {0, _T("\\")}, {MFT_ROOT_REC_ID, _T("C:\"")}, {0, _T("\"")},
         {0, _T("\\\\\\")}, {0, _T("users")}, { 0, _T("users\\default")},
         {0, _T("d:")}, {0, _T("d:\\windows")}, { 0, _T("C:\\win")},
-        {0, _T("C:\\windows\\4444\\5555\\5.txt")}, {0, _T("C:\\windows\\windows")},
+       /* {0, _T("C:\\windows\\4444\\5555\\5.txt")}, {0, _T("C:\\windows\\windows")},
         {0, _T("C:\\ProgramFiles")},{0, _T("C:\\Program  Files")},
         {58663, _T("C:\\windows")}, {58663, _T("c:\\WINDOWS")}, {58663, _T("C:\\WindowS")},
         {MFT_ROOT_REC_ID, _T("C:WindowS")}, {0, _T("D:WindowS")},
@@ -258,16 +309,60 @@ TEST_P(MFTPlainRecordsTest, DISABLED_GetMFTRecIdByPath_1)
         {405697, _T("c:\\Program Files\\Git\\mingw64\\libexec\\git-core\\git-column.exe")},
         {793004, _T("c:\\Windows\\WinSxS\\amd64_amdgpio2.inf.resources_31bf3856ad364e35_10.0.26100.1_ru-ru_973ad9b5977fab1e\\")},
         {98425, _T("c:\\Program Files\\Google\\Chrome\\Application")},
-        {98426, _T("c:\\Program Files\\Google\\Chrome\\Application\\SetupMetrics")},
+        {98426, _T("c:\\Program Files\\Google\\Chrome\\Application\\SetupMetrics")},*/
     };
 
     for (auto p : testData)
     {
-        EXPECT_EQ(p.first, ps.GetMFTRecIdByPath(p.second));
+        auto expctValue = ps.GetMFTRecIdByPath(p.second);
+        if (expctValue)
+            EXPECT_EQ(p.first, expctValue.value());
+        else
+            EXPECT_EQ(p.first, 0ul);
+            //EXPECT_TRUE(false) << "GetMFTRecIdByPath failed with error: " << ErrorCodeNames[(uint8_t)expctValue.error()];
     }
 
 }
 
-INSTANTIATE_TEST_CASE_P(PlainRecords1, MFTPlainRecordsTest, testing::Values(_T(TEST_DATA_DIR "compressed_sparse.mft")));
-INSTANTIATE_TEST_CASE_P(PlainRecords2, MFTPlainRecordsTest, testing::Values(_T(TEST_DATA_DIR "deleted.mft")));
-INSTANTIATE_TEST_CASE_P(PlainRecords3, MFTPlainRecordsTest, testing::Values(_T(TEST_DATA_DIR "different_la.mft")));
+// MFT IDs will be different for different disks, this test linked to disk C: only
+TEST_P(MFTPlainRecordsTest, GetPathByMFTRecId_1)
+{
+    auto& fileName = GetParam();
+    TMFTPlainRecordsLoader ldr(fileName);
+    TMFTParserBase ps(ldr);
+
+    THArray<std::pair<uint32_t, ci_string>> testData
+    {
+        {MFT_ROOT_REC_ID, _T("C:")},
+        {0, _T("c:\\$MFT")},
+        {1, _T("C:\\$MFTMirr")}, {2, _T("C:\\$LogFile")},
+        {3, _T("C:\\$Volume")},
+        {4, _T("c:\\$AttrDef")}, {6, _T("c:\\$Bitmap")},
+        {57651, _T("NOT_FOUND")},
+        {406846, _T("NOT_FOUND")},
+        /*{405697, _T("c:\\Program Files\\Git\\mingw64\\bin\\git.exe")},
+        {793004, _T("c:\\Windows\\WinSxS\\amd64_amdgpio2.inf.resources_31bf3856ad364e35_10.0.26100.1_ru-ru_973ad9b5977fab1e")},
+        {98425, _T("c:\\Program Files\\Google\\Chrome\\Application")},
+        {98426, _T("c:\\Program Files\\Google\\Chrome\\Application\\SetupMetrics")},*/
+
+    };
+
+    for (auto p : testData)
+    {
+        THArray<std::wstring> paths;
+        MFT_REF id{ p.first };
+        paths.Clear();
+        auto res = ps.GetPathByMFTRecID(id, paths);
+        if (res == TErrorCode::Success)
+            EXPECT_TRUE(EXPECT_ONE_OF(p.second, paths));
+        else if (res == TErrorCode::NotFound)
+            EXPECT_EQ(ci_string(_T("NOT_FOUND")), p.second);
+        else
+            FAIL() << "GetPathByMFTRecID returned error: " << ErrorCodeNames[(uint8_t)res];
+    }
+}
+
+
+INSTANTIATE_TEST_CASE_P(PlainRecords1, MFTPlainRecordsTest, testing::Values(COMPRESSED_SPARSE_FILE));
+INSTANTIATE_TEST_CASE_P(PlainRecords2, MFTPlainRecordsTest, testing::Values(DELETED_FILE));
+INSTANTIATE_TEST_CASE_P(PlainRecords3, MFTPlainRecordsTest, testing::Values(DIFFERENT_LA_FILE));
