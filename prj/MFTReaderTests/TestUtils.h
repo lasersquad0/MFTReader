43b7139fd16524842540049158d4eeaa5acaf88c
lasersquad0 <lasersquad@gmail.com>
2026-07-29 23:27:23 +0300

    template func EXPECT_ONE_OF is moved here

--- prj/MFTReaderTests/TestUtils.h
+++ prj/MFTReaderTests/TestUtils.h
@@ -59,3 +59,18 @@ struct NTFS_BOOT_SECTOR {
 static_assert(sizeof(NTFS_BOOT_SECTOR) == 84);
 
 #pragma pack(pop)
+
+template<typename STRING, typename ARRAY>
+bool EXPECT_ONE_OF(STRING expected, ARRAY arr)
+{
+	// make sure that STRING is one of instantiations of strings
+	static_assert(std::is_base_of<std::basic_string<typename STRING::value_type, typename STRING::traits_type>, STRING>::value);
+	// make sure that wstr is either std::string (no conversion required) or std::wstring
+	static_assert(std::is_same_v<typename STRING::value_type, wchar_t> || std::is_same_v<typename STRING::value_type, char>);
+	static_assert(std::is_same_v<typename STRING::value_type, typename ARRAY::item_type::value_type>);
+	//static_assert(std::is_convertible_v<typename ARRAY::item_type, STRING>);
+
+	for (auto& item : arr)
+		if (expected == STRING(item.c_str())) return true;
+	return false;
+}
