f0807e16b73bfc7085da15ea9977b48ee43f3f80
lasersquad0 <lasersquad@gmail.com>
2026-08-07 16:21:28 +0300

    minor changes

--- prj/MFTReaderTests/TestUtils.h
+++ prj/MFTReaderTests/TestUtils.h
@@ -68,11 +68,15 @@ bool EXPECT_ONE_OF(STRING expected, ARRAY arr)
 	static_assert(std::is_base_of<std::basic_string<typename STRING::value_type, typename STRING::traits_type>, STRING>::value);
 	// make sure that wstr is either std::string (no conversion required) or std::wstring
 	static_assert(std::is_same_v<typename STRING::value_type, wchar_t> || std::is_same_v<typename STRING::value_type, char>);
-	static_assert(std::is_same_v<typename STRING::value_type, typename ARRAY::item_type::value_type>);
+	static_assert(std::is_same_v<typename ARRAY::item_type::value_type, wchar_t> || std::is_same_v<typename ARRAY::item_type::value_type, char>);
+	//static_assert(std::is_same_v<typename STRING::value_type, typename ARRAY::item_type::value_type>);
 	//static_assert(std::is_convertible_v<typename ARRAY::item_type, STRING>);
-
+	
 	for (auto& item : arr)
-		if (expected == STRING(item.c_str())) return true;
+	{
+		auto str = convert_string<typename STRING::value_type>(item);
+		if (expected == str.c_str()) return true; //==STRING(str.c_str())
+	}
 	return false;
 }
 
