b9071404d63993c75d87c47071bb9bd36c10fb2f
lasersquad0 <lasersquad@gmail.com>
2026-08-10 12:38:49 +0300

    FileDateToString and TrimSPCRLF moved to functions.cpp from utils.cpp. WCHARToChar and CharToWCHAR - deleted. Commented out unused defines

--- include/Utils.h
+++ include/Utils.h
@@ -6,28 +6,19 @@
 #include "strutils/include/string_utils.h" // for string_t
 #include "logengine2/DynamicArrays.h" // for ulong type
 
-#define HIDWORD(_) static_cast<uint32_t>(((uint64_t)(_)) >> 32)
-#define LODWORD(_) static_cast<uint32_t>(_)
-
 //TODO think how can we report here error in general way for non-console applications
-#define LOG_CHECK_ERROR(_msg) std::cout << (_msg)
+//#define LOG_CHECK_ERROR(_msg) std::cout << (_msg)
 //#define LOG_CHECK_ERROR(_msg) logger.Error(_msg)
 
-#define MAKE_LOG_TSTR(_msg) string_t(_T(__FILE__)).append(_T(" : ")).append(__func__).append(_T(" : ")).append(std::to_string(__LINE__)).append(_T(" - ")).append(_msg)
-#define MAKE_LOG_STR(_msg) std::string(__FILE__).append(" : ").append(__func__).append(" : ").append(std::to_string(__LINE__)).append(" - ").append(_msg)
+//#define MAKE_LOG_TSTR(_msg) string_t(_T(__FILE__)).append(_T(" : ")).append(__func__).append(_T(" : ")).append(std::to_string(__LINE__)).append(_T(" - ")).append(_msg)
+//#define MAKE_LOG_STR(_msg) std::string(__FILE__).append(" : ").append(__func__).append(" : ").append(std::to_string(__LINE__)).append(" - ").append(_msg)
 
-#define HR_CHECK(_hr) {if(FAILED(_hr)) LOG_CHECK_ERROR( MAKE_LOG_STR(std::system_category().message(_hr)));}
-#define BOOL_CHECK(_res) {if(!(_res)) LOG_CHECK_ERROR( MAKE_LOG_STR(std::system_category().message(GetLastError())));}
+//#define HR_CHECK(_hr) {if(FAILED(_hr)) LOG_CHECK_ERROR( MAKE_LOG_STR(std::system_category().message(_hr)));}
+//#define BOOL_CHECK(_res) {if(!(_res)) LOG_CHECK_ERROR( MAKE_LOG_STR(std::system_category().message(GetLastError())));}
 
-string_t FileDateToString(const string_t& str, uint64_t dateTime);
 
-// removes all leading and trailing \n\t\r and space symbols from string
-std::wstring TrimSPCRLF(std::wstring str);
-
-void WCHARtoChar(char* dest, wchar_t* src);
-void CharToWCHAR(wchar_t* dest, const char* src);
 std::string GetErrorMessageTextA(ulong lastErrorCode, const std::string& errorPlace);
-string_t GetErrorMessageText(ulong lastErrorCode, const string_t& errorPlace);
+//string_t GetErrorMessageText(ulong lastErrorCode, const string_t& errorPlace);
 //void PrintWindowsErrorMessage(const std::string& functionName);
 
 
