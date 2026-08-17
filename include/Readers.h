0f06cefad045b68777b85f6c833952d6c02468f2
lasersquad0 <lasersquad@gmail.com>
2026-08-17 21:48:08 +0300

    TMFTParserBase class renamed to TMFTBaseReader. ReadMftItems(Info) changed parameters. Added PrintXXX method for printing certain attributes.

--- include/Readers.h
+++ include/Readers.h
@@ -9,16 +9,16 @@
 #define STREAM_NONAME "<noname>"
 #define STREAM_NONAME_W L"<noname>"
 
-class TMFTParserBase
+class TMFTBaseReader
 {
 protected:
 	IRecordsLoader& FLoader;
 	const VOLUME_DATA& getVolData() const { return FLoader.GetVolumeData(); }
+	ostream_t& FOut;
 public:
-	TMFTParserBase(IRecordsLoader& loader) : FLoader(loader) {};
+	TMFTBaseReader(IRecordsLoader& loader) : FOut(cout_t), FLoader(loader) {};
 
-	//bool FixupUSA(uint8_t* dataBuf, CLST startLCN, uint64_t iblocksCount, uint32_t indexBlockSize);
-	//void FillAttrValues(MFT_FILE_RECORD* mftRec, PMFT_ATTR_HEADER* attrValues);
+	//TErrorCode FillAttrCollection(MFT_REF mftRecRef, TAttrCollection& collection);
 	TErrorCode FillAttrCollection(MFT_FILE_RECORD* mftRec, TAttrCollection& collection);
 	TErrorCode FillAttrCollection(MFT_FILE_RECORD* mftRec, uint32_t attrFilter, TAttrCollection& collection);
 	//bool FillCollectionFromAttrList(MFTRecIndex indexMFTRec, uint32_t attrFilter, ATTR_LIST_ENTRY* startListItem, uint8_t* attrListEnd1, uint8_t* attrListEnd2, TAttrCollection& collection);
@@ -47,39 +47,52 @@ public:
 	TErrorCode GetFileListFromMFTRec(MFT_FILE_RECORD* mftRec, DIR_NODE& node);
 	TErrorCode PathByMFTRecID(MFT_REF mftRecRef, THArray<std::wstring>& paths);
 	std::expected<MFTRecIndex, TErrorCode> MFTRecIdByPath(const ci_string& path); // ci_string is for case INsensitive search here
+	
+	void PrintAttrCollection(TAttrCollection& collection);
+
+	TErrorCode PrintMFTRecord(MFT_FILE_RECORD* mftRec);
+	TErrorCode PrintSTDInfo(MFT_ATTR_HEADER* attr);
+	TErrorCode PrintFileNames(TAttrHeaderList& fileNamesList);
+	TErrorCode PrintDirectory(TAttrCollection& collection);
+	TErrorCode PrintObjectID(MFT_ATTR_HEADER* attr);
+	TErrorCode PrintLabel(MFT_ATTR_HEADER* attr);
+	TErrorCode PrintVolumeInfo(MFT_ATTR_HEADER* attr);
+	TErrorCode PrintDataInfo(MFT_ATTR_HEADER* attr);
 };
 
 typedef int32_t(*ReadMftItemsCallback)(const string_t& data);
 
-class TMFTStatCollector : public TMFTParserBase
+class TMFTStatCollector : public TMFTBaseReader
 {
 private:
 	TItemInfoList FItemsList;
 	THashUnordered<std::wstring, std::wstring> FStatistics;
 	bool FProcessNonResAttr; // whether to process non-resident attrs. for some tests it is not needed to process non-res attrs
+
+	int64_t CountFileNamesWithNameType(uint8_t nameType, uint32_t count);
 public:
-	TMFTStatCollector(IRecordsLoader& loader, bool processNonRes = true) : TMFTParserBase(loader), FProcessNonResAttr(processNonRes) {};
+	TMFTStatCollector(IRecordsLoader& loader, bool processNonRes = true) : TMFTBaseReader(loader), FProcessNonResAttr(processNonRes) {};
 	
 	TItemInfoList& GetItemsList() { return FItemsList; }
 
-	TErrorCode ReadMftItems(FILE_NAME fileItem,/*MFT_REF mftRecRef, MFT_REF parentMFTRecRef,*/ uint32_t dirLevel, ReadMftItemsCallback callback);
-	TErrorCode ReadMftItems(MFT_REF mftRecRef, uint32_t dirLevel, ReadMftItemsCallback callback);
-	TErrorCode ReadMftItemInfo(FILE_NAME fileItem, /*MFT_REF mftRecRef, MFT_REF parentMFTRecRef,*/ ITEM_INFO& itemInfo);
-	TErrorCode ReadMftItemInfo(MFT_REF mftRecRef, ITEM_INFO& itemInfo);
-	TErrorCode ReadMftItemInfoBuf(MFT_FILE_RECORD* mftRec, FILE_NAME fileItem, /*MFT_REF parentMFTRecRef,*/ ITEM_INFO& itemInfo);
-	TErrorCode ReadMftItemInfoBuf(MFT_FILE_RECORD* mftRec, ITEM_INFO& itemInfo);
+	TErrorCode ReadMftItems(MFT_REF mftRecRef, IFILE_NAME* iFileItem, uint32_t dirLevel, ReadMftItemsCallback callback);
+	//TErrorCode ReadMftItems(MFT_REF mftRecRef, uint32_t dirLevel, ReadMftItemsCallback callback);
+	TErrorCode ReadMftItemInfo(MFT_REF mftRecRef, IFILE_NAME* iFileItem, ITEM_INFO& itemInfo);
+	//TErrorCode ReadMftItemInfo(MFT_REF mftRecRef, ITEM_INFO& itemInfo);
+	TErrorCode ReadMftItemInfoBuf(MFT_FILE_RECORD* mftRec, IFILE_NAME* iFileItem, ITEM_INFO& itemInfo);
+	//TErrorCode ReadMftItemInfoBuf(MFT_FILE_RECORD* mftRec, ITEM_INFO& itemInfo);
 	TErrorCode CollectVolumeStat();
 	void ShowVolumeStat();
 	void SaveToFile(string_t fileName);
 
 };
 
-class TMFTSearchReader: public TMFTParserBase
+class TMFTSearchReader: public TMFTBaseReader
 {
 public:
 	TFileCache FFileList;
 
-	TMFTSearchReader(IRecordsLoader& loader) : TMFTParserBase(loader) { }
+	TMFTSearchReader(IRecordsLoader& loader) : TMFTBaseReader(loader) { }
 	TErrorCode ParseMFTRecord(uint8_t* mftRecData, DIR_NODE& node, AddFileAttrPred addToFileListPred /*uint32_t parentIdx, TFileCache::TLevel* level*/);
 
 	TErrorCode ReadDirectoryV1(uint32_t parentIdx, CACHE_ITEM* parentItem, uint64_t& dirSize, ProgressCallbackPtr callback);
@@ -87,12 +100,12 @@ public:
 	void SaveToFile(string_t fileName);
 };
 
-class TMFTSearchReaderV2 : public TMFTParserBase
+class TMFTSearchReaderV2 : public TMFTBaseReader
 {
 private:
 	TFileList FDirList;
 public:
-	TMFTSearchReaderV2(IRecordsLoader& loader) : TMFTParserBase(loader) { } 
+	TMFTSearchReaderV2(IRecordsLoader& loader) : TMFTBaseReader(loader) { } 
 
 	TErrorCode ReadDirectoryV2(MFT_REF parentMftRecID, uint32_t dirLevel);
 	void ReadDirsV2();
