733a5b29ef5b2ec5235a51be898d89a1be68e9d2
lasersquad0 <lasersquad@gmail.com>
2026-08-19 18:52:42 +0300

    .

--- include/MFTReader.h
+++ include/MFTReader.h
@@ -1,12 +1,11 @@
 #pragma once
 
 
-#define OPT_M _T("m") // "MFT Record ID"
-//#define OPT_V _T("v") // "Volume"
-#define OPT_P _T("p") // "Path"
-#define OPT_S _T("s") // "Statistic"
-#define OPT_C _T("c") // "Cache for file search"
-#define OPT_T _T("t") // "Testing" - for testing purposes
+#define OPT_R _T("r")   // "by Record ID"
+#define OPT_P _T("p")   // "by Path"
+#define OPT_S _T("s")   // "collect Statistic"
+#define OPT_C _T("c")   // "build Cache for file search"
+#define OPT_T _T("t")   // "Testing" - for testing purposes
 
 #define MFT_LOG_CFG_FILENAME "MFTReader.lfg"
 #define MFT_LOG_FILENAME "LogMFTReader.log"
