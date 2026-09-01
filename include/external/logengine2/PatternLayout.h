47ab0c7bb4876c246575e108ed1bcf6f5708d084
lasersquad0 <lasersquad@gmail.com>
2026-09-01 14:02:05 +0300

    Changes regarding to changing access path to Log Levels

--- include/external/logengine2/PatternLayout.h
+++ include/external/logengine2/PatternLayout.h
@@ -20,10 +20,10 @@ class PatternLayout : public Layout
 {
 protected:
 	// default pattern values
-	const std::string DefPatterns[Levels::n_LogLevels]{ DefaultLinePattern, DefaultCritPattern, DefaultErrorPattern,
+	const std::string DefPatterns[llnLogLevels]{ DefaultLinePattern, DefaultCritPattern, DefaultErrorPattern,
 				             DefaultWarningPattern, DefaultInfoPattern, DefaultDebugPattern, DefaultTracePattern };
 	// actual pattern values
-	Pattern MessagePatterns[Levels::n_LogLevels]{ Pattern{DefaultLinePattern}, Pattern{DefaultCritPattern}, Pattern{DefaultErrorPattern},
+	Pattern MessagePatterns[llnLogLevels]{ Pattern{DefaultLinePattern}, Pattern{DefaultCritPattern}, Pattern{DefaultErrorPattern},
 		          Pattern{DefaultWarningPattern}, Pattern{DefaultInfoPattern}, Pattern{DefaultDebugPattern}, Pattern{DefaultTracePattern} };
 	Pattern AppName{ DefaultAppName };
 	Pattern AppVersion{ DefaultAppVersion };
@@ -35,8 +35,8 @@ public:
 	PatternLayout() {}
 	~PatternLayout() override {}
 	std::string Format(const LogEvent& lv, Properties& props) override { return MessagePatterns[lv.msgLevel].Format(lv, props); }
-	std::string GetPattern(Levels::LogLevel level) override { return MessagePatterns[level].GetPattern(); }
-	std::string GetPattern() override { return MessagePatterns[Levels::llOff].GetPattern(); }
+	std::string GetPattern(LogLevel level) override { return MessagePatterns[level].GetPattern(); }
+	std::string GetPattern() override { return MessagePatterns[llOff].GetPattern(); }
 	
 	/*virtual void SetCritPattern (const std::string& pattern) { MessagePatterns[Levels::llCritical].SetPattern(pattern);}
 	virtual void SetErrorPattern(const std::string& pattern) { MessagePatterns[Levels::llError].SetPattern(pattern);   }
@@ -51,22 +51,22 @@ public:
 	virtual void SetAppName(const std::string& aname) { AppName.SetPattern(aname); }
 
 	// set pattern for specified LogLevel only
-	void SetPattern(const std::string& pattern, Levels::LogLevel level) override { MessagePatterns[level].SetPattern(pattern); }
+	void SetPattern(const std::string& pattern, LogLevel level) override { MessagePatterns[level].SetPattern(pattern); }
 
 	// set the same pattern for all lines: Crit, Error, Warn, Info, Debug, Trace
 	void SetPattern(const std::string& pattern) override
 	{
-		for (uint i = 0; i < Levels::n_LogLevels; i++)
+		for (uint i = 0; i < llnLogLevels; i++)
 			MessagePatterns[i].SetPattern(pattern);
 	}
 
 	// this is a way to "revert back" to default pattern for the specified log level
-	void SetDefaultPattern(Levels::LogLevel level) override { MessagePatterns[level].SetPattern(DefPatterns[level]); }
+	void SetDefaultPattern(LogLevel level) override { MessagePatterns[level].SetPattern(DefPatterns[level]); }
 
 	// this is a way to "revert back" to default patterns
 	void SetDefaultPattern() override
 	{
-		for (uint i = 0; i < Levels::n_LogLevels; i++)
+		for (uint i = 0; i < llnLogLevels; i++)
 			MessagePatterns[i].SetPattern(DefPatterns[i]);
 	}
 
