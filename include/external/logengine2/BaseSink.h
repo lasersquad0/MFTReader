2140f913aff06ebe7f9815d9a5dca134211c2652
lasersquad0 <lasersquad@gmail.com>
2026-09-01 13:52:01 +0300

    Access path to logging levels has simplified from LogEngine::Levels::llDebug to LogEngine::llDebug

--- include/external/logengine2/BaseSink.h
+++ include/external/logengine2/BaseSink.h
@@ -25,21 +25,17 @@ template<class Mutex>
 class BaseSink : public Sink
 {
 protected:
-	//std::string FName;
-	//Levels::LogLevel FLogLevel{ LL_DEFAULT };
 	Mutex FMutex;
 	Layout* FLayout{ nullptr };
-	ullong FMessageCounts[static_cast<int>(Levels::n_LogLevels)]{ 0,0,0,0,0,0 };
+	ullong FMessageCounts[static_cast<int>(llnLogLevels)]{ 0,0,0,0,0,0 };
 	ullong FBytesWritten = 0; //TODO may be move to Sink class?
 
 	//bool shouldLog(const Levels::LogLevel ll) const { return FLogLevel >= ll; }
 	//virtual void sendMsg(const LogEvent& e) = 0;
 
 public:
-	BaseSink(const std::string& name, const Levels::LogLevel ll = LL_DEFAULT) : Sink(name, ll), FLayout{ new PatternLayout() }
+	BaseSink(const std::string& name, const LogLevel ll = LL_DEFAULT) : Sink(name, ll), FLayout{ new PatternLayout() }
 	{
-		//for (uint i = 0; i < n_LogLevels; i++)
-		//	FMessageCounts[i] = 0;
 	}
 
 	//TODO add here constructor with PatternLayout parameter. Decide who should delete PatternLayout in this case.
@@ -80,7 +76,7 @@ public:
 	}
 
 	// sets log line pattern for the specified by parameter 'll' log line
-	void SetPattern(const std::string& pattern, Levels::LogLevel ll) override
+	void SetPattern(const std::string& pattern, LogLevel ll) override
 	{
 		std::lock_guard<Mutex> lock(FMutex);
 		FLayout->SetPattern(pattern, ll);
@@ -94,7 +90,7 @@ public:
 	}
 
 	// sets log line pattern for the specified by parameter 'll' log line
-	void SetDefaultPattern(Levels::LogLevel ll) override
+	void SetDefaultPattern(LogLevel ll) override
 	{
 		std::lock_guard<Mutex> lock(FMutex);
 		FLayout->SetDefaultPattern(ll);
