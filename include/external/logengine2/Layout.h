7e8929f909b38f372625b6d2483bb5d7021cdf14
lasersquad0 <lasersquad@gmail.com>
2026-09-01 13:59:08 +0300

    Changes regarding to changing access path to Log Levels

--- include/external/logengine2/Layout.h
+++ include/external/logengine2/Layout.h
@@ -23,20 +23,20 @@ public:
 	virtual ~Layout() { }
 	virtual std::string Format(const LogEvent& event, Properties& props) = 0;
 
-	virtual void SetPattern(const std::string&, Levels::LogLevel) = 0;
+	virtual void SetPattern(const std::string&, LogLevel) = 0;
 	virtual void SetPattern(const std::string&) = 0;
 	
-	virtual std::string GetPattern(Levels::LogLevel level) = 0;
+	virtual std::string GetPattern(LogLevel level) = 0;
 	virtual std::string GetPattern() = 0;
 
-	virtual void SetCritPattern (const std::string& pattern) { SetPattern(pattern, Levels::llCritical); }
-	virtual void SetErrorPattern(const std::string& pattern) { SetPattern(pattern, Levels::llError);    }
-	virtual void SetWarnPattern (const std::string& pattern) { SetPattern(pattern, Levels::llWarning);  }
-	virtual void SetInfoPattern (const std::string& pattern) { SetPattern(pattern, Levels::llInfo);     }
-	virtual void SetDebugPattern(const std::string& pattern) { SetPattern(pattern, Levels::llDebug);    }
-	virtual void SetTracePattern(const std::string& pattern) { SetPattern(pattern, Levels::llTrace);    }
+	virtual void SetCritPattern (const std::string& pattern) { SetPattern(pattern, llCritical); }
+	virtual void SetErrorPattern(const std::string& pattern) { SetPattern(pattern, llError);    }
+	virtual void SetWarnPattern (const std::string& pattern) { SetPattern(pattern, llWarning);  }
+	virtual void SetInfoPattern (const std::string& pattern) { SetPattern(pattern, llInfo);     }
+	virtual void SetDebugPattern(const std::string& pattern) { SetPattern(pattern, llDebug);    }
+	virtual void SetTracePattern(const std::string& pattern) { SetPattern(pattern, llTrace);    }
 
-	virtual void SetDefaultPattern(Levels::LogLevel level) = 0;
+	virtual void SetDefaultPattern(LogLevel level) = 0;
 	virtual void SetDefaultPattern() = 0;
 
 };
